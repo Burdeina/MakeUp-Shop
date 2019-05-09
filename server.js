dff964cfc41797b33ea094811bb0c558522263b7
Ira Burdeina <iruna.burdeina@gmail.com>
2019-05-10 00:52:46 +0300

    Add login

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
@@ -24,12 +27,32 @@ app.get('/index', function(req, res) {
 app.get('/error404', function(req, res) {
     res.render("error404");
 });
+
+app.get('/login', function(req, res) {
+    res.render("login");
+});
+
+app.get('/temp', function(req, res) {
+    res.render("temp");
+});
+
+app.post('/login', urlencodedParser, function(req, res) {
+    if (!req.body) return res.sendStatus(400);
+    res.render("login_success", {data: req.body});
+});
+app.get('/images/HTML-404-Error-Page.gif', function(req, res) {
+});
+
+app.get('/images/dior_lip_maximazer.jpg', function(req, res) {
+});
+
+
 app.get('/css/style.css', function(req, res) {
     //if (1) then use /static/css/style.css
 });
 
 app.get('/product/:id', function(req, res) {
-    var obj = {name: "lipstick", count: "7", producer: ["maybellen", "diva", "loreal","doir"]};
+    var obj = {name: "lipstick", count: "7", params: ["color:pink", "price 89$","producer:doir"]};
     res.render('product', {productId: req.params.id, obj: obj});
 });
 app.get('/scripts/errorMenuScript.js', function(req, res) {
